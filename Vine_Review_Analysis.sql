db697a5494f414a0e021fa8691c5f55131ad3190
Madison Pooser <m.pooser317@gmail.com>
2021-06-14 14:04:25 -0400

    SQL code performing analysis in vine_table

--- Vine_Review_Analysis.sql
+++ Vine_Review_Analysis.sql
@@ -17,16 +17,16 @@ select * into vine_table4 from vine_table2
     where vine = 'N';
 select * from vine_table4;
 select count(review_id) as "Count of Vine Reviews" from vine_table3;
-select count(review_id) as "Count of 5-Star Reviews" from vine_table3
+select count(review_id) as "Count of 5-Star Vine Reviews" from vine_table3
     where star_rating = 5;
-select star_rating, round(count(*) * 100.0 / (select count(*) from vine_table3),2) as "% of 5-Star Reviews" 
+select star_rating, round(count(*) * 100.0 / (select count(*) from vine_table3),2) as "% of 5-Star Vine Reviews" 
     from vine_table3
     where star_rating = 5
     group by star_rating;
 select count(review_id) as "Count of Non-Vine Reviews" from vine_table4;
-select count(review_id) as "Count of 5-Star Reviews" from vine_table4
+select count(review_id) as "Count of 5-Star Non-Vine Reviews" from vine_table4
     where star_rating = 5;
-select star_rating, round(count(*) * 100.0 / (select count(*) from vine_table4),2) as "% of 5-Star Reviews" 
+select star_rating, round(count(*) * 100.0 / (select count(*) from vine_table4),2) as "% of 5-Star Non-Vine Reviews" 
     from vine_table4
     where star_rating = 5
     group by star_rating;
